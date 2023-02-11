e0a7b3dfffb0f867a067fd8c98136686dad3050b
Dcruz05 <dcruz11@ucol.mx>
2023-02-10 18:43:06 -0600

    Prueba con modulo

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
+require("./greet")
+
 let greet = ()=> console.log("Hello")
-greet();
+
+logGreeting = miParametro => miParametro();
+logGreeting(greet);
+
+logGreeting(function (){
+    console.log(`Hello from a function created on the fly`)  
+})
+
+let color ="rojo";
+let talla = "M";
+let modelo = "Clasico";
+console.log(`Juan tiene una playera 
+talla ${talla} 
+de color ${color} 
+modelo ${modelo}`); //se pueden hacer salto de linea sin \n
